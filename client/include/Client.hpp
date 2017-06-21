ffa8cce4c1f9d10ae595c6007e554ea5e3765498
Remi Gastaldi <remi.gastaldi@epitech.eu>
2017-06-21 16:15:03 +0200

    [+] main [+] client class

--- client/include/Client.hpp
+++ client/include/Client.hpp
@@ -1,14 +1,26 @@
 //
 // Client.hpp for zappy in /home/gastal_r/rendu/Zappy
-// 
-// Made by 
+//
+// Made by
 // Login   <gastal_r>
-// 
-// Started on  Wed Jun 21 16:04:04 2017 
-// Last update Wed Jun 21 16:10:11 2017 
+//
+// Started on  Wed Jun 21 16:04:04 2017
+// Last update Wed Jun 21 16:10:11 2017
 //
 
 #ifndef		_CLIENT_HPP_
 #define		_CLIENT_HPP_
+
+class     Client
+{
+public:
+  Client();
+
+  ~Client() = default;
+  Client(const Client& other) = default;
+  Client(Client&& other) = default;
+  Client& operator=(const Client& other) = default;
+  Client& operator=(Client&& other) = default;
+};
 
 #endif	/* !_CLIENT_HPP_ */
---
+++ client/src/main.cpp
@@ -0,0 +1,19 @@
+//
+// main.cpp for Zappy in /home/gastal_r/rendu/Zappy/client/src/
+//
+// Made by gastal_r
+// Login   <remi.gastaldi@epitech.eu>
+//
+// Started on  Wed Jun 21 16:14:06 2017 gastal_r
+// Last update Wed Jun 21 16:14:39 2017 gastal_r
+//
+
+#include      "Client.hpp"
+
+int           main(int ac, char *av[])
+{
+  (void) ac;
+  (void) av;
+  
+  return (0);
+}
