3823dc0eb1de1f75a0264e35c258e0ce56d461ae
Remi Gastaldi <remi.gastaldi@epitech.eu>
2017-06-21 16:10:33 +0200

    [+] Client files

---
+++ client/include/Client.hpp
@@ -0,0 +1,14 @@
+//
+// Client.hpp for zappy in /home/gastal_r/rendu/Zappy
+// 
+// Made by 
+// Login   <gastal_r>
+// 
+// Started on  Wed Jun 21 16:04:04 2017 
+// Last update Wed Jun 21 16:10:11 2017 
+//
+
+#ifndef		_CLIENT_HPP_
+#define		_CLIENT_HPP_
+
+#endif	/* !_CLIENT_HPP_ */
---
+++ client/src/client.cpp
@@ -0,0 +1,9 @@
+//
+// client.cpp for zappy in /home/gastal_r/rendu/Zappy/client/src
+// 
+// Made by 
+// Login   <gastal_r>
+// 
+// Started on  Wed Jun 21 13:20:37 2017 
+// Last update Wed Jun 21 13:20:39 2017 
+//
