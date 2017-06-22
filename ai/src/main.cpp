c0ee43fe0a136e24b8855739da15a2ded182246b
Remi Gastaldi <remi.gastaldi@epitech.eu>
2017-06-22 18:57:02 +0200

    [+] ai init

--- ai/src/main.cpp
+++ ai/src/main.cpp
@@ -5,7 +5,7 @@
 // Login   <remi.gastaldi@epitech.eu>
 //
 // Started on  Wed Jun 21 16:14:06 2017 gastal_r
-// Last update Wed Jun 21 20:50:39 2017 gastal_r
+// Last update Thu Jun 22 18:55:14 2017 gastal_r
 //
 
 #include      "Ai.hpp"
@@ -14,18 +14,15 @@ int           main(int ac, char *av[])
 {
   (void) ac;
   (void) av;
-  Communication   communication;
 
   if (ac != 7)
   {
     std::cout << "USAGE: ./zappy_ai -p port -n name -h machine" << std::endl;
-    std::cout << "\tport is the port number" << std::endl;
-    std::cout << "\tname is the name of the team" << std::endl;
     std::cout << "\tmachine is the name of the machine; localhost by default" << std::endl;
+    std::cout << "\tname is the name of the team" << std::endl;
+    std::cout << "\tport is the port number" << std::endl;
   }
-  communication.setPort(atoi(av[2]));
-  communication.setMachine(av[6]);
-  communication.receiveCmd("forward\n");
-  // neverForgetTo(lock->Compter);
+  Ai ai;
+  ai.start();
   return (0);
 }
