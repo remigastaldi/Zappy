eb4a20e9c1110de3f76b1fb5a12c03bbb5cfc2b8
Frost <matthias.prost@epitech.eu>
2017-06-24 17:04:54 +0200

    [+] Add orientation commands

--- server/src/params_actions.c
+++ server/src/params_actions.c
@@ -5,22 +5,23 @@
 ** Login   <matthias.prost@epitech.eu@epitech.eu>
 **
 ** Started on  Fri Jun 23 15:43:06 2017 Matthias Prost
-** Last update Sat Jun 24 16:43:35 2017 Matthias Prost
+** Last update Sat Jun 24 17:04:06 2017 Matthias Prost
 */
 
 #include "server.h"
 
-void  forwardParam(t_env *env, char **msg, t_users *user)
+void  forwardYMoving(t_env *env, t_users *user)
 {
-  (void)msg;
   if (user->direction == UP)
   {
     if (user->posY == 0)
       user->posY = env->height - 1;
     else
       user->posY = user->posY - 1;
-    printf("User with socket %d go UP on position %dy : %dx\n",
+    printf("User with socket %d moved UP on position %dy : %dx\n",
           user->socket, user->posY, user->posX);
+    dprintf(user->socket, "ok\n");
+    printf("--> Sent: \"ok\" to socket %d\n", user->socket);
   }
   else if (user->direction == DOWN)
   {
@@ -28,17 +29,27 @@ void  forwardParam(t_env *env, char **msg, t_users *user)
       user->posY = 0;
     else
       user->posY = user->posY + 1;
-    printf("User with socket %d go DOWN on position %dy : %dx\n",
+    printf("User with socket %d moved DOWN on position %dy : %dx\n",
           user->socket, user->posY, user->posX);
+    dprintf(user->socket, "ok\n");
+    printf("--> Sent: \"ok\" to socket %d\n", user->socket);
   }
-  else if (user->direction == LEFT)
+}
+
+void  forwardParam(t_env *env, char **msg, t_users *user)
+{
+  (void)msg;
+  forwardYMoving(env, user);
+  if (user->direction == LEFT)
   {
     if (user->posX == 0)
       user->posX = env->width - 1;
     else
       user->posX = user->posX - 1;
-    printf("User with socket %d go LEFT on position %dy : %dx\n",
+    printf("User with socket %d moved LEFT on position %dy : %dx\n",
           user->socket, user->posY, user->posX);
+    dprintf(user->socket, "ok\n");
+    printf("--> Sent: \"ok\" to socket %d\n", user->socket);
   }
   else if (user->direction == RIGHT)
   {
@@ -46,23 +57,29 @@ void  forwardParam(t_env *env, char **msg, t_users *user)
       user->posX = 0;
     else
       user->posX = user->posX + 1;
-    printf("User with socket %d go RIGHT on position %dy : %dx\n",
+    printf("User with socket %d moved RIGHT on position %dy : %dx\n",
           user->socket, user->posY, user->posX);
+    dprintf(user->socket, "ok\n");
+    printf("--> Sent: \"ok\" to socket %d\n", user->socket);
   }
 }
 
 void  rightParam(t_env *env, char **msg, t_users *user)
 {
-  printf("DONE\n");
-  (void)env;
   (void)msg;
-  (void)user;
+  (void)env;
+  user->direction = RIGHT;
+  printf("User with socket %d turn 90 deg right\n", user->socket);
+  dprintf(user->socket, "ok\n");
+  printf("--> Sent: \"ok\" to socket %d\n", user->socket);
 }
 
 void  leftParam(t_env *env, char **msg, t_users *user)
 {
-  printf("DONE\n");
-  (void)env;
   (void)msg;
-  (void)user;
+  (void)env;
+  user->direction = LEFT;
+  printf("User with socket %d turn 90 deg left\n", user->socket);
+  dprintf(user->socket, "ok\n");
+  printf("--> Sent: \"ok\" to socket %d\n", user->socket);
 }
