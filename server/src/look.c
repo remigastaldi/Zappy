29a0362086d78caf3e208baf6f2c46198299699e
Cyril Puccio <cyril.puccio@epitech.eu>
2017-06-26 16:24:27 +0200

    Auto stash before merge of "master" and "origin/master"
    [~] Look Quasi Working

--- server/src/look.c
+++ server/src/look.c
@@ -38,24 +38,45 @@ char            *ressource_to_string(t_env *env, int x, int y)
   while (++i != nb_item)
     {
       if (tmp.linemate-- > 0)
-        str = my_strcat(str, "linemate, ");
+        str = my_strcat(str, "linemate");
       else if (tmp.deraumere-- > 0)
-        str = my_strcat(str, "deraumere, ");
+        str = my_strcat(str, "deraumere");
       else if (tmp.sibur-- > 0)
-        str = my_strcat(str, "sibur, ");
+        str = my_strcat(str, "sibur");
       else if (tmp.mendiane-- > 0)
-        str = my_strcat(str, "mendiane, ");
+        str = my_strcat(str, "mendiane");
       else if (tmp.phiras-- > 0)
-        str = my_strcat(str, "phiras, ");
+        str = my_strcat(str, "phiras");
       else if (tmp.thystame-- > 0)
-        str = my_strcat(str, "thystame, ");
+        str = my_strcat(str, "thystame");
+      (i + 1 != nb_item ? str = my_strcat(str, " ") : 0);
     }
   // (nb_item > 0 ? str = my_strcat(str, "\b\b]") : (str = my_strcat(str, "]")));
   printf("%s\n", str);
   return (str);
 }
 
-char            *check_case(t_env *env, char *pos, int *c, int i)
+int             calc_pos(t_env *env, int *x, int *y, int lvl)
+{
+  if (env->users->direction == 0)
+  {
+    (*x) = (*x) - lvl;
+    (*y) = (*y) - lvl;
+    printf("calcx: %d calcy: %d\n", *x, *y);
+  }
+  return (0);
+}
+
+int             change_case(t_env *env, int *x, int *y)
+{
+  (void)y;
+  if (env->users->direction == 0)
+    (*x) = (*x) + 1;
+  printf("changex: %d changey: %d\n", *x, *y);
+  return (0);
+}
+
+char            *check_case(t_env *env, char *pos, int i)
 {
   int           j;
   int           x;
@@ -64,57 +85,44 @@ char            *check_case(t_env *env, char *pos, int *c, int i)
   j = -1;
   x = env->users->posX;
   y = env->users->posY;
-  (void)pos;
-  (void)env;
+  printf("x: %d y: %d\n", x, y);
+  calc_pos(env, &x, &y, i);
   while (++j != (i * 2 + 1))
   {
-    // faire le if des diférentes directions
-    if (env->users->direction == 0)
-      {
-        pos = ressource_to_string(env, x, y);
-        printf("ressources %d\n", env->map[env->users->posX][env->users->posY].linemate);
-        // pos[*c] = malloc(sizeof(char) * )
-      }
-    // pos[*c] =
-    (*c)++;
+    change_case(env, &x, &y);
+    pos = my_strcat(pos, ressource_to_string(env, x, y));
+    pos = my_strcat(pos, ", ");
   }
-  printf("%d\n", *c);
   return (pos);
 }
 
-char            *final_output(t_env *env, char *pos, int lvl)
+char            *final_output(t_env *env, int lvl)
 {
+  char          *pos;
   int           i;
-  int           c;
-  int           x = env->users->posX;
-  int           y = env->users->posY;
 
-  (void)x;
-  (void)y;
-  i = c = 0;
-  while (++i != lvl)
+  i = 0;
+  pos = "";
+  while (++i != lvl + 1)
     {
-      pos = check_case(env, pos, &c, i);
+      pos = check_case(env, pos, i);
 
     }
-  printf("c: %d\n", c);
   return (pos);
 }
 
 int             cmd_look(t_env *env)
 {
-  char          *pos;
   char          *tmp;
   char          *final = malloc (100);
 
-  pos = "";
   env->users->lvl = 2;
   env->users->direction = 0;
-  env->users->posX = 0;
-  env->users->posY = 0;
+  env->users->posX = 5;
+  env->users->posY = 5;
   printf("userlvl: %d\n", env->users->lvl);
-  printf("pos: x: %d y: %d\n", env->users->posX, env->users->posY);
-  tmp = final_output(env, pos, env->users->lvl);
+  // printf("pos: x: %d y: %d\n", env->users->posX, env->users->posY);
+  tmp = final_output(env, env->users->lvl);
   sprintf(final, "[%s]\n", tmp);
   printf("%s\n", final);
   // free(final);
