f14ec2186c0ae11a6a7e1dc54d6b4f3c9f7c8631
Remi Gastaldi <remi.gastaldi@epitech.eu>
2017-06-28 16:41:00 +0200

    [+] refreshqueue invalid read fix

--- server/src/refresh_queue.c
+++ server/src/refresh_queue.c
@@ -5,7 +5,7 @@
 ** Login   <remi.gastaldi@epitech.eu>
 **
 ** Started on  Wed Jun 28 14:46:13 2017 gastal_r
-** Last update Wed Jun 28 16:10:30 2017 Leo Hubert Froideval
+** Last update Wed Jun 28 16:40:11 2017 gastal_r
 */
 
 #include      "utilities.h"
@@ -15,6 +15,7 @@ void          refresh_queue(t_env *env)
 {
   double      curr_clock;
   t_action    *action;
+  t_action    *tmp;
 
   action = env->queue->head;
   curr_clock = clock() / CLOCKS_PER_SEC;
@@ -22,9 +23,12 @@ void          refresh_queue(t_env *env)
   {
     if (curr_clock >= action->time_limit)
     {
+      tmp = action->next;
       (*action->p)(action->env, action->msg, action->user);
       deleteAction(env->queue, action);
+      action = tmp;
     }
-    action = action->next;
+    if (action != NULL)
+      action = action->next;
   }
 }
