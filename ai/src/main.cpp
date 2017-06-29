b722e3474c125c83df0a61c60fe21b929ffd3a58
august_t <thomas.augusto@epitech.eu>
2017-06-29 16:40:16 +0200

    [~] Arg checking modified

--- ai/src/main.cpp
+++ ai/src/main.cpp
@@ -30,36 +30,57 @@ int   is_alpha(char *str)
 int          check_arg(char **av)
 {
   std::string   tmp(av[1]);
+  bool          tiretp = 0;
+  bool          tiretn = 0;
+  bool          tireth = 0;
 
-  if (tmp != "-p")
-    {
-      std::cout << "USAGE: ./zappy_ai -p port -n name -h machine" << std::endl;
-      std::cout << "\tmachine is the name of the machine; localhost by default" << std::endl;
-      std::cout << "\tname is the name of the team" << std::endl;
-      std::cout << "\tport is the port number" << std::endl;
+  if (tmp == "-p")
+  {
+    tiretp = 1;
+    if (is_alpha(av[2]) == 1)
       return (1);
-    }
+  }
+  else if (tmp == "-h")
+    tireth = 1;
+  else if (tmp == "-n")
+    tiretn = 1;
+
   tmp = av[3];
-  if (tmp != "-n")
+  if (tmp == "-p")
   {
-    std::cout << "USAGE: ./zappy_ai -p port -n name -h machine" << std::endl;
-    std::cout << "\tmachine is the name of the machine; localhost by default" << std::endl;
-    std::cout << "\tname is the name of the team" << std::endl;
-    std::cout << "\tport is the port number" << std::endl;
-    return (1);
+    tiretp = 1;
+    if (is_alpha(av[4]) == 1)
+      return (1);
   }
+  else if (tmp == "-h")
+    tireth = 1;
+  else if (tmp == "-n")
+    tiretn = 1;
+
   tmp = av[5];
-  if (tmp != "-h")
+  if (tmp == "-p")
+  {
+    tiretp = 1;
+    if (is_alpha(av[6]) == 1)
+      return (1);
+  }
+  else if (tmp == "-h")
+    tireth = 1;
+  else if (tmp == "-n")
+    tiretn = 1;
+
+  if (tiretn == 1 && tiretp == 1 && tireth == 1)
+  {
+    return (0);
+  }
+  else
   {
     std::cout << "USAGE: ./zappy_ai -p port -n name -h machine" << std::endl;
     std::cout << "\tmachine is the name of the machine; localhost by default" << std::endl;
     std::cout << "\tname is the name of the team" << std::endl;
     std::cout << "\tport is the port number" << std::endl;
-    return (1);
+    return (0);
   }
-  if (is_alpha(av[2]) == 1)
-    return (1);
-  return (0);
 }
 
 int           main(int ac, char *av[])
