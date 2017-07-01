a85908ecd8e6081367194f825eaaf39c18e6b1c9
Frost <matthias.prost@epitech.eu>
2017-07-01 18:19:07 +0200

    [+] Map have to be at least 10x10

--- server/src/args_commands.c
+++ server/src/args_commands.c
@@ -38,12 +38,15 @@ void   widthArg(t_env *env, char **av, int i)
     while (av[i + 1][++a])
     {
       if (av[i + 1][a] > '9' || av[i + 1][a] < '0')
-        print_error("ERROR: The width must be a positive number");
+        print_error("ERROR: The width must be a positive number >= 10");
     }
-    env->width = atoi(av[i + 1]);
+    if (atoi(av[i + 1]) >= 10)
+      env->width = atoi(av[i + 1]);
+    else
+      print_error("ERROR: The width must be a positive number >= 10");
   }
   else
-    print_error("ERROR: The width must be a positive number");
+    print_error("ERROR: The width must be a positive number >= 10");
 }
 
 void   heightArg(t_env *env, char **av, int i)
@@ -56,12 +59,15 @@ void   heightArg(t_env *env, char **av, int i)
     while (av[i + 1][++a])
     {
       if (av[i + 1][a] > '9' || av[i + 1][a] < '0')
-        print_error("ERROR: The height must be a positive number");
+        print_error("ERROR: The height must be a positive number >= 10");
     }
-    env->height = atoi(av[i + 1]);
+    if (atoi(av[i + 1]) >= 10)
+      env->height = atoi(av[i + 1]);
+    else
+      print_error("ERROR: The height must be a positive number >= 10");
   }
   else
-    print_error("ERROR: The height must be a positive number");
+    print_error("ERROR: The height must be a positive number >= 10");
 }
 
 void    clientsNbArg(t_env *env, char **av, int i)
